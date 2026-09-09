609708d03b1bc3dc570e915c15f37439ae5b2e35
Oscar-1st <olimjonovogabekotabek@gmail.com>
2026-09-09 19:36:09 +0900

    feat: TASK-E completed

---
+++ train.js
@@ -0,0 +1,13 @@
+function getReverse(rev) {
+  let result = rev.split("").reverse().join("");
+  console.log(result);
+}
+getReverse("hello");
+getReverse("ogabek");
+getReverse("Assalomu alekum");
+getReverse("omadbek");
+
+// TASK E: 
+
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
