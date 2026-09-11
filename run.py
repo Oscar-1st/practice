216443534103c45a9cbb2ee3e88c1fd401fe7ea7
Oscar-1st <olimjonovogabekotabek@gmail.com>
2026-09-11 23:19:14 +0900

    feat: python system  variables & special methods via dudners

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print('Hello World')
+# Dunder __bultins__, __init__
+massage = "Python: Everything is object"
+print(massage)
+
+result = type(massage)
+print("result:", result)
+
+
+''' PYTHON built in tools:
+(1) TYPES -> int, float, str, list, dict
+(2) FUNCTIONS -> print, len, input, type
+(3) CONSTANTS -> True, False, None
+'''
+
+print(dir(__dict__))
