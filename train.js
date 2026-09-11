8fe54b7d947f0acd2271dabbf192e3513f00bca0
Oscar-1st <olimjonovogabekotabek@gmail.com>
2026-09-12 00:44:00 +0900

    feat: TASK-F completed

--- train.js
+++ train.js
@@ -1,11 +1,34 @@
-function getReverse(rev) {
-  let result = rev.split("").reverse().join("");
-  console.log(result);
+function findDoublers(str) {
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) !== i) {
+      return true;
+    }
+  }
+
+  return false;
 }
-getReverse("hello");
-getReverse("ogabek");
-getReverse("Assalomu alekum");
-getReverse("omadbek");
+const result = findDoublers("hello");
+console.log(result);
+
+
+// TASK F:
+
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin.
+
+// MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+// function getReverse(rev) {
+//   let result = rev.split("").reverse().join("");
+//   console.log(result);
+// }
+// getReverse("hello");
+// getReverse("ogabek");
+// getReverse("Assalomu alekum");
+// getReverse("omadbek");
 
 // TASK E: 
 
